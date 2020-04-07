f1ce8db983d01bbcd71ba4be8bf2106c21e4ee20
Louis <louis@Louiss-Mac-mini.local>
2020-04-07 10:37:03 -0400

    Added material-ui, cards and graph

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 class App extends Component {
+  state = {
+    data: {},
+  };
   async componentDidMount() {
-    const data = fetchData();
-    console.log(data);
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
 
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/api/index.js
+++ src/api/index.js
@@ -18,3 +18,18 @@ export const fetchData = async () => {
     return modifiedData;
   } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    console.log(data, 'pre');
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {}
+};
