403228ea7c407311e3ce1557a6282fd5595cf03e
Louis <louis@Louiss-Mac-mini.local>
2020-04-07 12:12:08 -0400

    v1 complete. Added Bar chart

--- src/App.js
+++ src/App.js
@@ -4,24 +4,38 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import coronaImage from './images/c19-image.png';
+
 class App extends Component {
   state = {
     data: {},
+    country: '',
   };
+
   async componentDidMount() {
     const fetchedData = await fetchData();
 
     this.setState({ data: fetchedData });
   }
 
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({
+      data: fetchedData,
+      country,
+    });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} alt="COVID-19" src={coronaImage} />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
